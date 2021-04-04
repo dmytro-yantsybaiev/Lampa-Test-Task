a801a57def6b9215b8038decf2fab008bffa03a5
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-04-04 11:45:36 +0300

    Now tab buttons change their color according to which one was selected. Tab's scroll view now changes its X position to selected tab

--- Lampa-Test-Task/Controllers/MainViewController.swift
+++ Lampa-Test-Task/Controllers/MainViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    @IBOutlet weak var storiesTabButton: UIButton!
+    @IBOutlet weak var videoTabButton: UIButton!
+    @IBOutlet weak var favouritesTabButton: UIButton!
     @IBOutlet weak var tabsScrollView: UIScrollView!
     @IBOutlet weak var tabsPageControl: UIPageControl!
     
+    lazy var tabButtons = [storiesTabButton, videoTabButton, favouritesTabButton]
+    var currentTabPage: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +27,49 @@ class MainViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
           return .lightContent
     }
+    
+    @IBAction func tabButtonPressed(_ sender: UIButton) {
+        setAllTabButtonsCollorGray()
+        setSelectedTabButtonCollorWhite(sender)
+        
+        UIView.animate(withDuration: 0.2) {
+            self.changeScrollViewPositionToSelectedTab(sender)
+        }
+        
+    }
+    
+    func setAllTabButtonsCollorGray() {
+        for tabButton in tabButtons {
+            tabButton?.setTitleColor(.gray, for: .normal)
+        }
+    }
+    
+    func setSelectedTabButtonCollorWhite(_ button: UIButton) {
+        button.setTitleColor(.white, for: .normal)
+    }
+    
+    func changeScrollViewPositionToSelectedTab(_ tabButton: UIButton) {
+        currentTabPage = CGFloat(tabButtons.firstIndex(of: tabButton)!)
+        tabsScrollView.contentOffset.x = view.frame.width * currentTabPage
+    }
 }
 
 //MARK: - UIScrollViewDelegate
 
 extension MainViewController: UIScrollViewDelegate {
-    
+        
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        currentTabPage = pageIndex
         tabsPageControl.currentPage = Int(pageIndex)
+        
+        setAllTabButtonsCollorGray()
+        if pageIndex == 0 {
+            setSelectedTabButtonCollorWhite(tabButtons[0]!)
+        } else if pageIndex == 1 {
+            setSelectedTabButtonCollorWhite(tabButtons[1]!)
+        } else if pageIndex == 2 {
+            setSelectedTabButtonCollorWhite(tabButtons[2]!)
+        }
     }
 }
